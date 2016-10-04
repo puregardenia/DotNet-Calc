8cfeea6e24355fb838fc2c26c8f4c8e17abf3d2b
puregardenia <puregardenia@qq.com>
2016-10-04 18:29:21 +0800

    Init respo

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Text_Box1 = new System.Windows.Forms.TextBox();
+            this.button_7 = new System.Windows.Forms.Button();
+            this.button_8 = new System.Windows.Forms.Button();
+            this.button_9 = new System.Windows.Forms.Button();
+            this.button_4 = new System.Windows.Forms.Button();
+            this.button_5 = new System.Windows.Forms.Button();
+            this.button_6 = new System.Windows.Forms.Button();
+            this.button_1 = new System.Windows.Forms.Button();
+            this.button_2 = new System.Windows.Forms.Button();
+            this.button_3 = new System.Windows.Forms.Button();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_red = new System.Windows.Forms.Button();
+            this.button_mul = new System.Windows.Forms.Button();
+            this.button_div = new System.Windows.Forms.Button();
+            this.button_equ = new System.Windows.Forms.Button();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.查看ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.复制ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.粘贴ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Text_Box1
+            // 
+            this.Text_Box1.Location = new System.Drawing.Point(13, 34);
+            this.Text_Box1.Name = "Text_Box1";
+            this.Text_Box1.Size = new System.Drawing.Size(186, 21);
+            this.Text_Box1.TabIndex = 0;
+            this.Text_Box1.TextChanged += new System.EventHandler(this.Text_Box1_TextChanged);
+            // 
+            // button_7
+            // 
+            this.button_7.Location = new System.Drawing.Point(13, 78);
+            this.button_7.Name = "button_7";
+            this.button_7.Size = new System.Drawing.Size(37, 23);
+            this.button_7.TabIndex = 1;
+            this.button_7.Text = "7";
+            this.button_7.UseVisualStyleBackColor = true;
+            this.button_7.Click += new System.EventHandler(this.button_7_Click);
+            // 
+            // button_8
+            // 
+            this.button_8.Location = new System.Drawing.Point(61, 78);
+            this.button_8.Name = "button_8";
+            this.button_8.Size = new System.Drawing.Size(37, 23);
+            this.button_8.TabIndex = 2;
+            this.button_8.Text = "8";
+            this.button_8.UseVisualStyleBackColor = true;
+            this.button_8.Click += new System.EventHandler(this.button_8_Click);
+            // 
+            // button_9
+            // 
+            this.button_9.Location = new System.Drawing.Point(109, 78);
+            this.button_9.Name = "button_9";
+            this.button_9.Size = new System.Drawing.Size(37, 23);
+            this.button_9.TabIndex = 3;
+            this.button_9.Text = "9";
+            this.button_9.UseVisualStyleBackColor = true;
+            this.button_9.Click += new System.EventHandler(this.button_9_Click);
+            // 
+            // button_4
+            // 
+            this.button_4.Location = new System.Drawing.Point(13, 107);
+            this.button_4.Name = "button_4";
+            this.button_4.Size = new System.Drawing.Size(37, 23);
+            this.button_4.TabIndex = 4;
+            this.button_4.Text = "4";
+            this.button_4.UseVisualStyleBackColor = true;
+            this.button_4.Click += new System.EventHandler(this.button_4_Click);
+            // 
+            // button_5
+            // 
+            this.button_5.Location = new System.Drawing.Point(61, 107);
+            this.button_5.Name = "button_5";
+            this.button_5.Size = new System.Drawing.Size(37, 23);
+            this.button_5.TabIndex = 5;
+            this.button_5.Text = "5";
+            this.button_5.UseVisualStyleBackColor = true;
+            this.button_5.Click += new System.EventHandler(this.button_5_Click);
+            // 
+            // button_6
+            // 
+            this.button_6.Location = new System.Drawing.Point(109, 107);
+            this.button_6.Name = "button_6";
+            this.button_6.Size = new System.Drawing.Size(37, 23);
+            this.button_6.TabIndex = 6;
+            this.button_6.Text = "6";
+            this.button_6.UseVisualStyleBackColor = true;
+            this.button_6.Click += new System.EventHandler(this.button_6_Click);
+            // 
+            // button_1
+            // 
+            this.button_1.Location = new System.Drawing.Point(13, 136);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(37, 23);
+            this.button_1.TabIndex = 7;
+            this.button_1.Text = "1";
+            this.button_1.UseVisualStyleBackColor = true;
+            this.button_1.Click += new System.EventHandler(this.button_1_Click);
+            // 
+            // button_2
+            // 
+            this.button_2.Location = new System.Drawing.Point(61, 136);
+            this.button_2.Name = "button_2";
+            this.button_2.Size = new System.Drawing.Size(37, 23);
+            this.button_2.TabIndex = 8;
+            this.button_2.Text = "2";
+            this.button_2.UseVisualStyleBackColor = true;
+            this.button_2.Click += new System.EventHandler(this.button_2_Click);
+            // 
+            // button_3
+            // 
+            this.button_3.Location = new System.Drawing.Point(109, 136);
+            this.button_3.Name = "button_3";
+            this.button_3.Size = new System.Drawing.Size(37, 23);
+            this.button_3.TabIndex = 9;
+            this.button_3.Text = "3";
+            this.button_3.UseVisualStyleBackColor = true;
+            this.button_3.Click += new System.EventHandler(this.button_3_Click);
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(158, 78);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(41, 23);
+            this.button_add.TabIndex = 10;
+            this.button_add.Text = "+";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_red
+            // 
+            this.button_red.Location = new System.Drawing.Point(158, 107);
+            this.button_red.Name = "button_red";
+            this.button_red.Size = new System.Drawing.Size(41, 23);
+            this.button_red.TabIndex = 11;
+            this.button_red.Text = "-";
+            this.button_red.UseVisualStyleBackColor = true;
+            this.button_red.Click += new System.EventHandler(this.button_red_Click);
+            // 
+            // button_mul
+            // 
+            this.button_mul.Location = new System.Drawing.Point(158, 136);
+            this.button_mul.Name = "button_mul";
+            this.button_mul.Size = new System.Drawing.Size(41, 23);
+            this.button_mul.TabIndex = 12;
+            this.button_mul.Text = "*";
+            this.button_mul.UseVisualStyleBackColor = true;
+            this.button_mul.Click += new System.EventHandler(this.button_mul_Click);
+            // 
+            // button_div
+            // 
+            this.button_div.Location = new System.Drawing.Point(158, 165);
+            this.button_div.Name = "button_div";
+            this.button_div.Size = new System.Drawing.Size(41, 23);
+            this.button_div.TabIndex = 13;
+            this.button_div.Text = "/";
+            this.button_div.UseVisualStyleBackColor = true;
+            this.button_div.Click += new System.EventHandler(this.button_div_Click);
+            // 
+            // button_equ
+            // 
+            this.button_equ.Location = new System.Drawing.Point(77, 165);
+            this.button_equ.Name = "button_equ";
+            this.button_equ.Size = new System.Drawing.Size(65, 23);
+            this.button_equ.TabIndex = 14;
+            this.button_equ.TabStop = false;
+            this.button_equ.Text = "=";
+            this.button_equ.UseVisualStyleBackColor = true;
+            this.button_equ.Click += new System.EventHandler(this.button_equ_Click);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(13, 164);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(37, 23);
+            this.button_clear.TabIndex = 15;
+            this.button_clear.Text = "C";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.查看ToolStripMenuItem,
+            this.编辑ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(214, 24);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 查看ToolStripMenuItem
+            // 
+            this.查看ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关于ToolStripMenuItem});
+            this.查看ToolStripMenuItem.Name = "查看ToolStripMenuItem";
+            this.查看ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.查看ToolStripMenuItem.Text = "查看";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.关于ToolStripMenuItem.Text = "关于";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            // 
+            // 编辑ToolStripMenuItem
+            // 
+            this.编辑ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.复制ToolStripMenuItem,
+            this.粘贴ToolStripMenuItem});
+            this.编辑ToolStripMenuItem.Name = "编辑ToolStripMenuItem";
+            this.编辑ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.编辑ToolStripMenuItem.Text = "编辑";
+            // 
+            // 复制ToolStripMenuItem
+            // 
+            this.复制ToolStripMenuItem.Name = "复制ToolStripMenuItem";
+            this.复制ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.复制ToolStripMenuItem.Text = "复制";
+            this.复制ToolStripMenuItem.Click += new System.EventHandler(this.复制ToolStripMenuItem_Click);
+            // 
+            // 粘贴ToolStripMenuItem
+            // 
+            this.粘贴ToolStripMenuItem.Name = "粘贴ToolStripMenuItem";
+            this.粘贴ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.粘贴ToolStripMenuItem.Text = "粘贴";
+            this.粘贴ToolStripMenuItem.Click += new System.EventHandler(this.粘贴ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 205);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_equ);
+            this.Controls.Add(this.button_div);
+            this.Controls.Add(this.button_mul);
+            this.Controls.Add(this.button_red);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.button_3);
+            this.Controls.Add(this.button_2);
+            this.Controls.Add(this.button_1);
+            this.Controls.Add(this.button_6);
+            this.Controls.Add(this.button_5);
+            this.Controls.Add(this.button_4);
+            this.Controls.Add(this.button_9);
+            this.Controls.Add(this.button_8);
+            this.Controls.Add(this.button_7);
+            this.Controls.Add(this.Text_Box1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pure Calc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Text_Box1;
+        private System.Windows.Forms.Button button_7;
+        private System.Windows.Forms.Button button_8;
+        private System.Windows.Forms.Button button_9;
+        private System.Windows.Forms.Button button_4;
+        private System.Windows.Forms.Button button_5;
+        private System.Windows.Forms.Button button_6;
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.Button button_2;
+        private System.Windows.Forms.Button button_3;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_red;
+        private System.Windows.Forms.Button button_mul;
+        private System.Windows.Forms.Button button_div;
+        private System.Windows.Forms.Button button_equ;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 查看ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 编辑ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 复制ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 粘贴ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_1_Click(object sender, EventArgs e)
+        {
+            string bt1;
+            bt1 = button_1.Text;
+            Text_Box1.Text = Text_Box1.Text + bt1;
+        }
+
+        private void button_2_Click(object sender, EventArgs e)
+        {
+            string bt2;
+            bt2 = button_2.Text;
+            Text_Box1.Text = Text_Box1.Text + bt2;
+
+        }
+
+        private void button_3_Click(object sender, EventArgs e)
+        {
+            string bt3;
+            bt3 = button_3.Text;
+            Text_Box1.Text = Text_Box1.Text + bt3;
+
+        }
+
+        private void button_4_Click(object sender, EventArgs e)
+        {
+            string bt4;
+            bt4 = button_4.Text;
+            Text_Box1.Text = Text_Box1.Text + bt4;
+
+        }
+
+        private void button_5_Click(object sender, EventArgs e)
+        {
+            string bt5;
+            bt5 = button_5.Text;
+            Text_Box1.Text = Text_Box1.Text + bt5;
+
+        }
+
+        private void button_6_Click(object sender, EventArgs e)
+        {
+            string bt6;
+            bt6 = button_6.Text;
+            Text_Box1.Text = Text_Box1.Text + bt6;
+
+        }
+
+        private void button_7_Click(object sender, EventArgs e)
+        {
+            string bt7;
+            bt7 = button_7.Text;
+            Text_Box1.Text = Text_Box1.Text + bt7;
+
+        }
+
+        private void button_8_Click(object sender, EventArgs e)
+        {
+            string bt8;
+            bt8 = button_8.Text;
+            Text_Box1.Text = Text_Box1.Text + bt8;
+        }
+
+        private void button_9_Click(object sender, EventArgs e)
+        {
+            int bt9;
+            bt9 = int.Parse(button_9.Text);
+            Text_Box1.Text = Text_Box1.Text + bt9.ToString();
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            if (Text_Box1.Text.Contains("+") || Text_Box1.Text.Contains("-") || Text_Box1.Text.Contains("*") || Text_Box1.Text.Contains("/"))
+                button_equ_Click(null, null);
+            Text_Box1.Text = Text_Box1.Text + button_add.Text;
+        }
+
+        private void button_red_Click(object sender, EventArgs e)
+        {
+            if (Text_Box1.Text.Contains("+") || Text_Box1.Text.Contains("-") || Text_Box1.Text.Contains("*") || Text_Box1.Text.Contains("/"))
+                button_equ_Click(null, null);
+            Text_Box1.Text = Text_Box1.Text + button_red.Text;
+        }
+
+        private void button_mul_Click(object sender, EventArgs e)
+        {
+            if (Text_Box1.Text.Contains("+") || Text_Box1.Text.Contains("-") || Text_Box1.Text.Contains("*") || Text_Box1.Text.Contains("/"))
+                button_equ_Click(null, null);
+            Text_Box1.Text = Text_Box1.Text + button_mul.Text;
+        }
+
+        private void button_div_Click(object sender, EventArgs e)
+        {
+            if (Text_Box1.Text.Contains("+") || Text_Box1.Text.Contains("-") || Text_Box1.Text.Contains("*") || Text_Box1.Text.Contains("/"))
+                button_equ_Click(null, null);
+            Text_Box1.Text = Text_Box1.Text + button_div.Text;
+        }
+
+        private void button_equ_Click(object sender, EventArgs e)
+        {
+            if (Text_Box1.Text.Contains("+"))
+            {
+                int n = Text_Box1.Text.IndexOf("+");
+                int first = int.Parse(Text_Box1.Text.Substring(0, n));
+                int second = int.Parse(Text_Box1.Text.Substring(n + 1));
+                Text_Box1.Clear();
+                Text_Box1.Text = (first + second).ToString();
+            }
+            else if (Text_Box1.Text.Contains("-"))
+            {
+                int n = Text_Box1.Text.IndexOf('-');
+                long first = long.Parse(Text_Box1.Text.Substring(0, n));    //避免字符过长，超出Int范围
+                long second = long.Parse(Text_Box1.Text.Substring(n + 1));
+                Text_Box1.Clear();
+                Text_Box1.Text = (first - second).ToString();
+            }
+            else if (Text_Box1.Text.Contains("*"))
+            {
+                int n = Text_Box1.Text.IndexOf('*');
+                int first = int.Parse(Text_Box1.Text.Substring(0, n));
+                int second = int.Parse(Text_Box1.Text.Substring(n + 1));
+
+                Text_Box1.Clear();
+                Text_Box1.Text = (first * second).ToString();
+            }
+            else if (Text_Box1.Text.Contains("/"))
+            {
+                int n = Text_Box1.Text.IndexOf('/');
+                float first = int.Parse(Text_Box1.Text.Substring(0, n));        //float(除法)
+                float second = int.Parse(Text_Box1.Text.Substring(n + 1));
+                Text_Box1.Clear();
+                Text_Box1.Text = (first / second).ToString();
+            }
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            Text_Box1.Clear();
+        }
+
+        private void 复制ToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            if (Text_Box1.SelectedText.Equals(""))
+                return;
+            Clipboard.SetDataObject(Text_Box1.SelectedText,true);
+        }
+
+        private void 粘贴ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.GetDataObject().GetDataPresent(DataFormats.Text))
+            {
+                this.粘贴ToolStripMenuItem.Enabled = true;
+                this.Text_Box1.Paste();
+            }
+            else
+            {
+                this.粘贴ToolStripMenuItem.Enabled = false;   
+            }
+        }
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Powered by puregardenia","版权");
+        }
+
+        private void Text_Box1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
